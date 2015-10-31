96c27a22e33cd42a21f8b94fef653560de07c8f1
Ernest <ernest.rekel@gmail.com>
2015-10-31 16:32:10 +0200

    Main FOSUserBundle configuration

---
+++ src/Illuminati/UserBundle/Entity/User.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Illuminati\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    /**
+     * @ORM\Column(name="deleted", type="smallint", options={ "default" = 0 })
+     */
+    protected $deleted;
+
+    /**
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(name="surname", type="string", length=255)
+     */
+    protected $surname;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->deleted = 0;
+        // your own logic
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * @param string $surname
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDeleted()
+    {
+        return $this->deleted;
+    }
+
+    /**
+     * @param int $deleted
+     */
+    public function setDeleted($deleted)
+    {
+        $this->deleted = $deleted;
+    }
+
+
+}
